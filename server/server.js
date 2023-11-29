9f3ada03f14345241112986b25ab6ca23610d973
abdulmoeezhassan <121342680+abdulmoeezhassan@users.noreply.github.com>
2023-11-29 10:53:43 +0500

    api's of vocher man

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT=8000;
+const PORT=9000;
 
 //Sales Invoice...
 const  salesinvoiceRoute=require("./routes/salesmanagement/salesInvoice");
@@ -21,6 +21,39 @@ const SalesQuotationInvoice=require('./routes/salesmanagement/salequotation.js')
 //Sales Order Invoice...
 const SalesOrderInvoice=require('./routes/salesmanagement/saleorder.js');
 
+//Purchase Inovice...
+
+const PurcahseInvoice=require('./routes/purchasemanagement/purchaseInvoice.js');
+
+//Purchase Order Invoice...
+
+const PurcahseOrderInvoice=require('./routes/purchasemanagement/purchaseOrder.js');
+
+//Purchase Return....
+
+const PurcahseReturn=require('./routes/purchasemanagement/purchaseReturn.js');
+
+//Journal Voucher...
+
+const JournalVocher=require('./routes/voucherManagement/journalVoucher.js');
+
+//Bank Payment...
+
+const BankPayment=require('./routes/voucherManagement/BankPayement.js');
+
+//Cash Recipt...
+
+const CashRecipt=require('./routes/voucherManagement/CashRecipt.js');
+
+//Un Posted Voucher...
+
+const unPostedVoucher=require('./routes/voucherManagement/unPostedVoucher.js');
+
+//Un Reconciled Voucher...
+
+const reConciledVoucher=require('./routes/voucherManagement/UnReconicledVocher.js');
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //Sales Invoice...
 app.use('/salesInvoice',salesinvoiceRoute);
 
@@ -36,12 +69,33 @@ app.use('/SalesQuotationInvoice',SalesQuotationInvoice);
 //Sales Order Invoice...
 app.use('/SalesOrderInovice',SalesOrderInvoice);
 
+//Purchase Invoice...
+app.use('/PurchaseInvoice',PurcahseInvoice);
 
+//Purchase Order...
+app.use('/PurchaseOrder',PurcahseOrderInvoice);
 
+//Purchase Return...
+app.use('/PurchaseReturn',PurcahseReturn);
 
+//JournalVoucher...
 
+app.use('/JournalVoucher',JournalVocher);
 
+//BankPayment...
 
+app.use('/BankPayment',BankPayment);
+
+//Cash Recipt...
+
+app.use('/CashReceipt',CashRecipt);
+
+//Un posted Voucher...
+app.use('/UnPostedvoucher',unPostedVoucher);
+
+//Un Reconciled Voucher...
+app.use('/UnReconciledVoucher',reConciledVoucher);
+//Server...
 
 app.listen(PORT,()=>{
     console.log(`Server is listening on PORT ${PORT}`);
